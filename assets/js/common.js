0592576e08e32bce73da24c4eac32d608b80ccaa
Ioannis Mavromatis <ioan.mavromatis@gmail.com>
2020-08-01 00:11:30 +0100

    First version

---
+++ assets/js/common.js
@@ -0,0 +1,21 @@
+$(document).ready(function () {
+    $('a.abstract').click(function () {
+        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+    });
+    $('a.bibtex').click(function () {
+        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+    });
+
+    $('a.copybutton').click(function (event) {
+        var buttonid = event.target.id;
+        var range = document.createRange();
+        var padid = "copy-";
+        var newid = padid.concat(buttonid);
+
+        var range = document.createRange();
+        range.selectNode(document.getElementById(newid));
+        window.getSelection().addRange(range);
+        document.execCommand("copy");
+        window.getSelection().removeAllRanges();// to deselect
+    });
+});

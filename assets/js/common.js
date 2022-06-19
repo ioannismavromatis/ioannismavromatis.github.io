373113a92aada62eb8a98f3cb5ead5f94c62c515
Ioannis Mavromatis <ioan.mavromatis@gmail.com>
2022-06-19 01:57:41 +0100

    Update website with dark theme -- and new project, papers and datasets

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,38 +1,20 @@
-$(document).ready(function() {
+$(document).ready(function () {
     $('a.abstract').click(function () {
-        $(this).parent().parent().parent().find(".abstract.hidden").toggleClass('open');
+        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
     });
     $('a.bibtex').click(function () {
-        $(this).parent().parent().parent().find(".bibtex.hidden").toggleClass('open');
+        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
     });
-
-    $('a.copybutton').click(function (event) {
-        var buttonid = event.target.id;
-        var range = document.createRange();
-        var padid = "copy-";
-        var newid = padid.concat(buttonid);
-
-        var range = document.createRange();
-        range.selectNode(document.getElementById(newid));
-        window.getSelection().addRange(range);
-        document.execCommand("copy");
-        window.getSelection().removeAllRanges();// to deselect
-        $(this).text('Copied!'); 
-    });
-
+    $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
     $('div.card-project').click(function () {
         $(this).find(".hidden").toggleClass('open');
     });
-
-    $('a.datasetbutton').click(function () {
+    $('button.datasetbutton').click(function () {
         $(this).parent().parent().find(".hidden").toggleClass('open');
     });
-
-    $('#download').on('click', function(evt) {
+    $('#download').on('click', function (evt) {
         evt.preventDefault(); //don't submit the form, which a button naturally does
         if (confirm('The database is very large! Are you sure you want to continue?"'))
             location.href = 'https://ioannismavromatis.com/dataset'; //redirect only on confirm
     });
-
-    $('.navbar-nav').find('a').removeClass('waves-effect waves-light');
 });

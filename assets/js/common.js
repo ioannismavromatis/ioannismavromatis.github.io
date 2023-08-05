1e73cbd66405a7c9ee7f6cfa7cd1d18c19817c16
ioannismavromatis <ioan.mavromatis@gmail.com>
2023-08-05 16:48:13 +0100

    Updated website

--- assets/js/common.js
+++ assets/js/common.js
@@ -1,1 +1,60 @@
-$(document).ready(function(){$("a.abstract").click(function(){$(this).parent().parent().find(".abstract.hidden").toggleClass("open"),$(this).parent().parent().find(".bibtex.hidden.open").toggleClass("open")}),$("a.bibtex").click(function(){$(this).parent().parent().find(".bibtex.hidden").toggleClass("open"),$(this).parent().parent().find(".abstract.hidden.open").toggleClass("open")}),$("a").removeClass("waves-effect waves-light"),$("div.card-project").click(function(){$(this).find(".hidden").toggleClass("open")}),$("button.datasetbutton").click(function(){$(this).parent().parent().find(".hidden").toggleClass("open")}),$("#download").on("click",function(t){t.preventDefault(),confirm('The database is very large! Are you sure you want to continue?"')&&(location.href="https://ioannismavromatis.com/dataset")})}),$(document).ready(function(){if($("#toc-sidebar").length){var t="#toc-sidebar",e=$(t);Toc.init(e),$("body").scrollspy({target:t})}});
+$(document).ready(function() {
+    $('a.abstract').click(function() {
+        $(this).parent().parent().find(".abstract.hidden").toggleClass('open');
+        $(this).parent().parent().find(".bibtex.hidden.open").toggleClass('open');
+    });
+    $('a.bibtex').click(function () {
+        $(this).parent().parent().find(".bibtex.hidden").toggleClass('open');
+        $(this).parent().parent().find(".abstract.hidden.open").toggleClass('open');
+    });
+    $('a').removeClass('waves-effect waves-light');
+    $('div.card-project').click(function () {
+        $(this).find(".hidden").toggleClass('open');
+    });
+    $('button.datasetbutton').click(function () {
+        $(this).parent().parent().find(".hidden").toggleClass('open');
+    });
+    $('#download').on('click', function (evt) {
+        evt.preventDefault(); //don't submit the form, which a button naturally does
+        if (confirm('The database is very large! Are you sure you want to continue?"'))
+            location.href = 'https://ioannismavromatis.com/dataset'; //redirect only on confirm
+    });
+});
+
+  // bootstrap-toc
+  if($('#toc-sidebar').length){
+    var navSelector = "#toc-sidebar";
+    var $myNav = $(navSelector);
+    Toc.init($myNav);
+    $("body").scrollspy({
+      target: navSelector,
+    });
+  }
+
+  // add css to jupyter notebooks
+  const cssLink = document.createElement("link");
+  cssLink.href  = "../css/jupyter.css";
+  cssLink.rel   = "stylesheet";
+  cssLink.type  = "text/css";
+
+  let theme = localStorage.getItem("theme");
+  if (theme == null || theme == "null") {
+    const userPref = window.matchMedia;
+    if (userPref && userPref("(prefers-color-scheme: dark)").matches) {
+      theme = "dark";
+    }
+  }
+
+  $('.jupyter-notebook-iframe-container iframe').each(function() {
+    $(this).contents().find("head").append(cssLink);
+
+    if (theme == "dark") {
+      $(this).bind("load",function(){
+        $(this).contents().find("body").attr({
+          "data-jp-theme-light": "false",
+          "data-jp-theme-name": "JupyterLab Dark"});
+      });
+    }
+  });
+});
+
